4993c99297b2f06f63851674de2e852beafef586
Sebastian Zelonka <dev@sebazelonka.com>
2021-12-22 15:50:45 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
+import { App } from "./App";
+import { About } from "./routes/about";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
