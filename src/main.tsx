8c2acb9638b6d6ad08c6aaad5a57dd1d39633384
Sebastian Zelonka <dev@sebazelonka.com>
2021-12-22 16:35:25 -0300

    rename home + removed unused elements

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
-import { App } from "./App";
+import { App } from "./routes/home";
 import { About } from "./routes/about";
 
 ReactDOM.render(
